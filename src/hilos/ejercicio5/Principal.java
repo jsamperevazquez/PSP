d135fcd0b5a9d08e1145f0bfc097e0502145fca2
JSampereVázquez <jsamperevazquez@danielcastelao.org>
2021-10-15 09:40:53 +0200

    Usado MAXPRIORITY en setPriority

--- src/hilos/ejercicio5/Principal.java
+++ src/hilos/ejercicio5/Principal.java
@@ -8,7 +8,7 @@ public class Principal {
     public static void main(String[] args) throws InterruptedException {
         Hilos hilo1 = new Hilos("número1");
         Hilos hilo2 = new Hilos("número2");
-        hilo2.setPriority(10);
+        hilo2.setPriority(Thread.MAX_PRIORITY);
         hilo2.start();
         hilo2.join();
         hilo1.start();
